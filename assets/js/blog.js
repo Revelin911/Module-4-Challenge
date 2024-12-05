5e0fcbaf7995e504856e356ef6c0abf9886cbcf3
Revelin911 <revelin911@mail.com>
2024-12-04 19:35:14 -0700

    blog.js

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,24 +1,29 @@
 // TODO: Create a variable that selects the main element, and a variable that selects the back button element
+const article = document.querySelector('article');
 const main = document.querySelector('main');
 const backButton = document.querySelector('back-button');
+
 // TODO: Create a function that builds an element and appends it to the DOM
-function buildElement(blog) {
-    const username = document.createElement('username');
-    const title = document.createElement('title');
-    const content = document.createElement('content');
-    const blockquote = document.createElement('blockquote');
-    username.textContent = username;
-    title.textContent = title;
-    content.textContent = content;
-    article.append(username, title, content, blockquote);
+
+function buildElement() {
+
+    const title = document.createElement('h2');
+    const content = document.createElement('p');
+    const username = document.createElement('blockquote');
+    blog.title = title;
+    blog.content = content;
+    blog.username = username;
+    article.append(title, content, username);
     main.append(article);
 }
 // TODO: Create a function that handles the case where there are no blog posts to display
+
 function noPosts() {
-    const p = document.createElement('p');
+    
     p.textContent = 'No posts to display';
 }
 // TODO: Create a function called `renderBlogList` that renders the list of blog posts if they exist. If not, call the no posts function.
+
 function renderBlogList() {
     const blogData = readLocalStorage();
     if (blogData.length === 0) {
